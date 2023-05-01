dc4da842c84309f6d6ad2c2aa5cd9599e92eaa25
Giancarl0s <giancarloshernaiz16@gmail.com>
2023-05-01 12:03:52 -0400

    prueba

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ import modulos.agregar as agregar
 import modulos.listar as listar
 
 #Prueba 
+#Prueba 2
 def menu() -> None:
     # Funcion que muestra el menu principal, consta del bucle principal y la seleccion de opciones
     listaProductos = []
