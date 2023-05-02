820f49c30aba427f91fc3e6b29f90367e58acb92
Giancarl0s <giancarloshernaiz16@gmail.com>
2023-05-02 19:01:06 -0400

    Listar

--- src/modulos/listar.py
+++ src/modulos/listar.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 
 def listarProductos(lista: list[str]) -> None:
+    lista_aux = []
     cantidad = []
     pesos = []
     fechas_creacion = []
@@ -8,16 +9,9 @@ def listarProductos(lista: list[str]) -> None:
     dimensiones = []
     for i in range (0, len(lista)):
         cantidad.append(lista[i]["Cantidad"])
-        fecha_temp = lista[i]["Fecha Creacion"].split("-")
-        fechas_temp = datetime(int(fecha_temp[2]), int(fecha_temp[1]), int(fecha_temp[0])).date()
-        fechas_creacion.append(fechas_temp)
-        if lista[i]["Fecha Modificacion"] == "Sin Modificaciones":
-            fechas_modificacion.append(datetime.strptime(lista[i]["Fecha Creacion"], '%d-%m-%Y').date())
-        else: 
-            fechas_modificacion.append(datetime.strptime(lista[i]["Fecha Modificacion"], '%d-%m-%Y').date())
-        dim = lista[i]["Dimensiones"].split("x")
-        dimensiones.append(int(dim[0]) * int(dim[1]))
-    
+        fechas_creacion.append(datetime.strptime(lista[i]["Fecha Creacion"], '%d-%m-%Y').date())
+        fechas_modificacion.append(datetime.strptime(lista[i]["Fecha Modificacion"], '%d-%m-%Y').date())
+
     print()
     print("+---------------------------+ Opciones de listado +---------------------------+")
     print("| 1-. Según su cantidad (ascendente - quicksort)                              |") 
@@ -47,53 +41,40 @@ def listarProductos(lista: list[str]) -> None:
                         break
             
         elif opcion == 2:
-
+            lista_aux.clear()
             while True:
                 fecha_desde = input(">>> Fecha desde (dia-mes-año): ")
                 fecha_hasta = input(">>> Fecha hasta (dia-mes-año): ")
+                print()
                 try:
                     fecha_desde = datetime.strptime(fecha_desde, '%d-%m-%Y').date()
                     fecha_hasta = datetime.strptime(fecha_hasta, '%d-%m-%Y').date()
+                    
                     break;
                 except:
                     print("-> Formato de fecha incorrecto (dd-mm-yyyy)...")
 
             fechas_rango = list(fechas_modificacion)
             for i in range (len(fechas_modificacion)):
-                fecha_prod = fechas_modificacion[i]
-                if (fecha_hasta.year < fecha_prod.year):
-                    fechas_rango.remove(fecha_prod)
-                elif (fecha_hasta.year == fecha_prod.year):
-                    if (fecha_hasta.month < fecha_prod.month):
-                        fechas_rango.remove(fecha_prod)
-                    elif (fecha_hasta.month == fecha_prod.month) :
-                        if (fecha_hasta.day < fecha_prod.day):
-                            fechas_rango.remove(fecha_prod)
-                if (fecha_desde.year > fecha_prod.year):
-                    fechas_rango.remove(fecha_prod)
-                elif (fecha_desde.year == fecha_prod.year):
-                    if (fecha_desde.month > fecha_prod.month):
-                        fechas_rango.remove(fecha_prod)
-                    elif (fecha_desde.month == fecha_prod.month) :
-                        if (fecha_desde.day > fecha_prod.day):
-                            fechas_rango.remove(fecha_prod)
-            for i in range(len(fechas_rango) ):
-                for j in range(len(lista) ):
-                    fecha_c_temp = lista[j]["Fecha Creacion"].split("-")
-                    fecha_m_temp = lista[j]["Fecha Creacion"].split("-")
-                    if lista[j]["Fecha Modificacion"] == "Sin Modificaciones":
-                        if fechas_rango[i] == datetime(int(fecha_c_temp[2]), int(fecha_c_temp[1]), int(fecha_c_temp[0])).date():
-                            pesos.append(lista[j]["Peso"])
-                    else:
-                        if fechas_rango == datetime(int(fecha_m_temp[2]), int(fecha_m_temp[1]), int(fecha_m_temp[0])).date():
-                            pesos.append(lista[j]["Peso"])
+                if (fecha_hasta < fechas_modificacion[i] or fecha_desde > fechas_modificacion[i]):
+                    fechas_rango.remove(fechas_modificacion[i])
             
-            pesos = mergesort(pesos) # fechas_modi
-            for i in range(len(pesos) ):
+            for i in range(len(fechas_rango)):
                 for j in range(len(lista) ):
-                    if pesos[i] == lista[j]["Peso"]:
-                        print(str(i) + " " + str(lista[j]))
-                        break
+                    if fechas_rango[i] == datetime.strptime(lista[j]["Fecha Modificacion"], '%d-%m-%Y').date():
+                        pesos.append(lista[j]["Peso"])
+                        lista_aux.append(lista[j])
+                        
+                        break;
+            
+            pesos = mergesort(pesos)
+            reps = []
+            for i in range(len(pesos) ):
+                for j in range(len(lista_aux)):
+                    if pesos[i] == lista_aux[j]["Peso"] and lista_aux[j]["Nombre"] not in reps:
+                        print(str(i) + " " + str(lista_aux[j]))
+                        reps.append(lista_aux[j]["Nombre"])
+                        break;
 
         elif opcion == 3:
 
@@ -110,21 +91,14 @@ def listarProductos(lista: list[str]) -> None:
                 print("-> No se ha ingresado un numero, seleccione una opcion valida\n")
             if not error:
                 if opcion == 1:           
-                    fechas_creacion = shellsort(fechas_creacion, 0)
-                    for i in range(len(fechas_creacion) ):
-                        for j in range(len(lista) ):
-                            fecha_temp = lista[j]["Fecha Creacion"].split("-")
-                            if fechas_creacion[i] ==  datetime(int(fecha_temp[2]), int(fecha_temp[1]), int(fecha_temp[0])).date():
-                                print(str(i) + " " + str(lista[j]))
-                                break
-                elif opcion == 2:
                     fechas_creacion = shellsort(fechas_creacion, 1)
-                    for i in range(len(fechas_creacion) ):
-                        for j in range(len(lista) ):
-                            fecha_temp = lista[j]["Fecha Creacion"].split("-")
-                            if fechas_creacion[i] ==  datetime(int(fecha_temp[2]), int(fecha_temp[1]), int(fecha_temp[0])).date():
-                                print(str(i) + " " + str(lista[j]))
-                                break
+                elif opcion == 2:
+                    fechas_creacion = shellsort(fechas_creacion, 0)
+                for i in range(len(fechas_creacion) ):
+                    for j in range(len(lista) ):
+                        if fechas_creacion[i] == datetime.strptime(lista[j]["Fecha Creacion"], '%d-%m-%Y').date():
+                            print(str(i) + " " + str(lista[j]))
+                            break;
         
         elif opcion == 4:
             cantidad = heapsort(cantidad)
@@ -135,7 +109,8 @@ def listarProductos(lista: list[str]) -> None:
                         break
         
         elif opcion == 5: 
-            
+
+            lista_aux.clear()
             while True:
                 fecha_desde = input(">>> Fecha desde (dia-mes-año): ")
                 fecha_hasta = input(">>> Fecha hasta (dia-mes-año): ")
@@ -145,37 +120,12 @@ def listarProductos(lista: list[str]) -> None:
                     break;
                 except:
                     print("-> Formato de fecha incorrecto (dd-mm-yyyy)...")
-            fechas_rango = []
             fechas_rango = list(fechas_creacion)
             for i in range (len(fechas_creacion)):
-                fecha_prod = fechas_creacion[i]
-                if (fecha_hasta.year < fecha_prod.year):
-                    fechas_rango.remove(fecha_prod)
-                elif (fecha_hasta.year == fecha_prod.year):
-                    if (fecha_hasta.month < fecha_prod.month):
-                        fechas_rango.remove(fecha_prod)
-                    elif (fecha_hasta.month == fecha_prod.month) :
-                        if (fecha_hasta.day < fecha_prod.day):
-                            fechas_rango.remove(fecha_prod)
-                if (fecha_desde.year > fecha_prod.year):
-                    fechas_rango.remove(fecha_prod)
-                elif (fecha_desde.year == fecha_prod.year):
-                    if (fecha_desde.month > fecha_prod.month):
-                        fechas_rango.remove(fecha_prod)
-                    elif (fecha_desde.month == fecha_prod.month) :
-                        if (fecha_desde.day > fecha_prod.day):
-                            fechas_rango.remove(fecha_prod)
-
+                if (fecha_hasta < fechas_creacion[i] or fecha_desde > fechas_creacion[i]):
+                    fechas_rango.remove(fechas_creacion[i])
 
             '''Arreglo y condicionales'''
-
-
-
-
-
-
-
-
 
 
             print("+---+ Opciones de listado +---+")
@@ -190,11 +140,11 @@ def listarProductos(lista: list[str]) -> None:
                 error = True
                 print("-> No se ha ingresado un numero, seleccione una opcion valida\n")
             if not error:
-                if opcion == 1:
-                    print("+---+ Opciones de listado +---+")
-                    print("| 1-. Ascendente              |")
-                    print("| 2-. Descendente             |")
-                    print("+-----------------------------+")
+                print("+---+ Opciones de listado +---+")
+                print("| 1-. Ascendente              |")
+                print("| 2-. Descendente             |")
+                print("+-----------------------------+")
+                if opcion == 1: #Dimensiones
                     try:
                         opcion = int(input("-> Opcion deseada: "))
                         error = opcion not in range(1, 3)
@@ -203,27 +153,28 @@ def listarProductos(lista: list[str]) -> None:
                         error = True
                         print("-> No se ha ingresado un numero, seleccione una opcion valida\n")
                     if not error:
-                        if opcion == 1:
-                            dimensiones = shellsort(dimensiones, 0)
-                            for i in range(len(dimensiones) ):
-                                for j in range(len(lista) ):
+
+                        for i in range(len(fechas_rango) ):
+                            for j in range(len(lista) ):
+                                if fechas_rango[i] == datetime.strptime(lista[j]["Fecha Creacion"], '%d-%m-%Y').date():
                                     dim = lista[j]["Dimensiones"].split("x")
-                                    if dimensiones[i] == (int(dim[0]) * int(dim[1])):
-                                        print(str(i) + " " + str(lista[j]))
-                                        break
-                        elif opcion == 2:
-                            dimensiones = shellsort(dimensiones, 1)        
-                            for i in range(len(dimensiones) ):
-                                for j in range(len(lista) ):
-                                    dim = lista[j]["Dimensiones"].split("x")
-                                    if dimensiones[i] == (int(dim[0]) * int(dim[1])):
-                                        print(str(i) + " " + str(lista[j]))   
-                                        break     
-                elif opcion == 2:
-                    print("+---+ Opciones de listado +---+")
-                    print("| 1-. Ascendente              |")
-                    print("| 2-. Descendente             |")
-                    print("+-----------------------------+")
+                                    dimensiones.append(int(dim[0]) * int(dim[1]))
+                                    lista_aux.append(lista[j])
+
+                        if opcion == 1: #Ascendente 1
+                            dimensiones = quicksort(dimensiones)
+                        elif opcion == 2: #Descendente 0
+                            dimensiones = mergesort(dimensiones)       
+                        reps = []
+                        for i in range(len(dimensiones)):
+                            for j in range(len(lista_aux) ):
+                                dim = lista_aux[j]["Dimensiones"].split("x")
+                                if dimensiones[i] == (int(dim[0]) * int(dim[1])) and lista_aux[j]["Nombre"] not in reps:
+                                    print(str(i) + " " + str(lista_aux[j]))
+                                    reps.append(lista_aux[j]["Nombre"])
+                                    break;              
+                
+                elif opcion == 2: #Antiguedad
                     try:
                         opcion = int(input("-> Opcion deseada: "))
                         error = opcion not in range(1, 3)
@@ -232,22 +183,16 @@ def listarProductos(lista: list[str]) -> None:
                         error = True
                         print("-> No se ha ingresado un numero, seleccione una opcion valida\n")
                     if not error:
+                        fechas_creacion.clear()
                         if opcion == 1:           
-                            fechas_creacion = shellsort(fechas_creacion, 0)
-                            for i in range(len(fechas_creacion) ):
-                                for j in range(len(lista) ):
-                                    fecha_temp = lista[j]["Fecha Creacion"].split("-")
-                                    if fechas_creacion[i] ==  datetime(int(fecha_temp[2]), int(fecha_temp[1]), int(fecha_temp[0])).date():
-                                        print(str(i) + " " + str(lista[j]))
-                                        break
+                            fechas_creacion = shellsort(fechas_rango, 1)
                         elif opcion == 2:
-                            fechas_creacion = shellsort(fechas_creacion, 1)
-                            for i in range(len(fechas_creacion) ):
-                                for j in range(len(lista) ):
-                                    fecha_temp = lista[j]["Fecha Creacion"].split("-")
-                                    if fechas_creacion[i] ==  datetime(int(fecha_temp[2]), int(fecha_temp[1]), int(fecha_temp[0])).date():
-                                        print(str(i) + " " + str(lista[j]))
-                                        break
+                            fechas_creacion = shellsort(fechas_rango, 0)
+                        for i in range(len(fechas_creacion) ):
+                            for j in range(len(lista) ):
+                                if fechas_creacion[i] ==  datetime.strptime(lista[j]["Fecha Creacion"], '%d-%m-%Y').date():
+                                    print(str(i) + " " + str(lista[j]))
+                                    break;
         
         else:
             pass
@@ -295,21 +240,17 @@ def mergesort(pesos):
             k += 1
     return pesos
 
-def shellsort(fechas, orden=0):
+def shellsort(fechas, orden):
     n = len(fechas)
     intervalo = n // 2
     while intervalo > 0:
         for i in range(intervalo, n):
             temp = fechas[i]
             j = i
-            if orden == 0:
-                while j >= intervalo and fechas[j-intervalo] > temp:
-                    fechas[j] = fechas[j-intervalo]
-                    j -= intervalo
-            else:
-                while j >= intervalo and fechas[j-intervalo] < temp:
-                    fechas[j] = fechas[j-intervalo]
-                    j -= intervalo
+            while j >= intervalo and ((orden == 1 and fechas[j - intervalo] > temp) or (orden == 0 and fechas[j - intervalo] < temp)):
+                fechas[j] = fechas[j - intervalo]
+                j -= intervalo
+
             fechas[j] = temp
         intervalo //= 2
     return fechas
@@ -338,10 +279,3 @@ def heapsort(cantidad):
         cantidad[i], cantidad[0] = cantidad[0], cantidad[i]
         heapify(cantidad, i, 0)
     return cantidad
-
-def algoritmo(productos):
-    # Ascendente o descendente segun sus dimensiones y antiguedad en un rango de fecha
-    print("+---+ Opciones de listado +---+")
-    print("| 1-. Ascendente              |")
-    print("| 2-. Descendente             |")
-    print("+-----------------------------+")
